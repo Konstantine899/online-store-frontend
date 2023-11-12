ce8de6b0840287019f0fbf362246cb7cbe9932e2
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-11-12 15:26:12 +0300

    feat: настроил поддержку ts в проекте

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    module:{
+        rules:[{test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/}]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'build'),
         clean: true
     },
+
     plugins: [new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')})],
 }
