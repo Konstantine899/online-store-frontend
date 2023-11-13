63c9213c2c398390330178ec5b76ab49c9b61498
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-11-13 10:17:53 +0300

    feat: добавил поддержку css modules в webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { Counter } from "./Counter";
+import "./index.scss";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<Counter />);
+root.render(
+  <div className='app'>
+    <Counter />
+  </div>
+);
