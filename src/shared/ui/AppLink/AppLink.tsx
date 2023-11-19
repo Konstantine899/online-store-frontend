e6cb053a9e84cc6aee97f17655b92a9d04d99680
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-11-19 14:32:36 +0300

    feat: добавил UIKit компонент AppLink

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,23 @@
+import { Link, LinkProps } from "react-router-dom";
+import { ReactNode } from "react";
+import { classNames } from "../../lib/classNames/classNames";
+import cls from "./AppLink.module.scss";
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  children?: ReactNode;
+}
+
+export const AppLink = (props: AppLinkProps) => {
+  const { children, className, to, ...otherProps } = props;
+
+  return (
+    <Link
+      className={classNames(cls.AppLink, {}, [])}
+      to={to}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,8 @@
 import { memo } from "react";
 import { classNames } from "../../../shared/lib/classNames/classNames";
-import { Link } from "react-router-dom";
 import cls from "./Navbar.module.scss";
 import { publicRoutePath } from "../../../app/providers/router/config/publicRouterConfig";
+import { AppLink } from "../../../shared/ui/AppLink/AppLink";
 
 interface NavbarProps {
   className?: string;
@@ -13,10 +13,10 @@ export const Navbar = memo((props: NavbarProps) => {
 
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <Link to={publicRoutePath.main}>Магазин</Link>
+      <AppLink to={publicRoutePath.main}>Магазин</AppLink>
       <div className={cls.publicLink}>
-        <Link to={publicRoutePath.auth}>Войти</Link>
-        <Link to={publicRoutePath.sign_up}>Регистрация</Link>
+        <AppLink to={publicRoutePath.auth}>Войти</AppLink>
+        <AppLink to={publicRoutePath.sign_up}>Регистрация</AppLink>
       </div>
     </div>
   );
