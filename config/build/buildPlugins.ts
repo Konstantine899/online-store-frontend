af781199561e849f3cc45785fe53c58f8e76425d
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-11-20 16:26:48 +0300

    feat: проверку типов вынес в отдельный процесс ForkTsCheckerWebpackPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,11 +2,13 @@ import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 
 export function buildPlugins({
   paths,
+  isDev,
 }: BuildOptions): webpack.ProgressPlugin[] {
-  return [
+  const plugins = [
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({
       template: paths.html,
@@ -15,6 +17,18 @@ export function buildPlugins({
       filename: "css/[name].[contenthash:8].css",
       chunkFilename: "css/[name].[contenthash:8].css",
     }),
-    new webpack.HotModuleReplacementPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        diagnosticOptions: {
+          semantic: true,
+          syntactic: true,
+        },
+        mode: "write-references",
+      },
+    }),
   ];
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+  return plugins;
 }
