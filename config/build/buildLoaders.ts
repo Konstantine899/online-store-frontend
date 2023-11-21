f2fe970eeb50bce2ff2024e645464ee4316313b7
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-11-21 09:49:23 +0300

    feat: добавил fileLoader и svgLoader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,12 +1,16 @@
 import webpack from "webpack";
-import { BuildOptions } from "./types/config";
-import { buildCssLoader } from "./loaders/buildCssLoader";
-import { buildBabelLoader } from "./loaders/buildBabelLoader";
+import {BuildOptions} from "./types/config";
+import {buildCssLoader} from "./loaders/buildCssLoader";
+import {buildBabelLoader} from "./loaders/buildBabelLoader";
+import {buildSvgLoader} from "./loaders/buildSvgLoader";
+import {buildFileLoader} from "./loaders/buildFileLoader";
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-  const { isDev } = options;
-  const tsBabelLoader = buildBabelLoader({ ...options, isTsx: false });
-  const tsxBabelLoader = buildBabelLoader({ ...options, isTsx: true });
-  const cssLoader = buildCssLoader(isDev);
-  return [tsBabelLoader, tsxBabelLoader, cssLoader];
+    const {isDev} = options;
+    const tsBabelLoader = buildBabelLoader({...options, isTsx: false});
+    const tsxBabelLoader = buildBabelLoader({...options, isTsx: true});
+    const cssLoader = buildCssLoader(isDev);
+    const svgLoader = buildSvgLoader();
+    const fileLoader = buildFileLoader();
+    return [tsBabelLoader, tsxBabelLoader, svgLoader, fileLoader, cssLoader];
 }
