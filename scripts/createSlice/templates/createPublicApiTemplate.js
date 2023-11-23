8c23eb086d0586e34b7c4649b7425514b4cf9982
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-11-23 11:02:31 +0300

    refactor: createPublicApiTemplate.js изменил import type schema

--- scripts/createSlice/templates/createPublicApiTemplate.js
+++ scripts/createSlice/templates/createPublicApiTemplate.js
@@ -1,4 +1,4 @@
 module.exports = function (componentName, schemaName) {
   return `export { ${componentName} } from './ui/${componentName}/${componentName}';
-  export { ${schemaName} } from './model/types/${schemaName}';`;
+  export type { ${schemaName} } from './model/types/${schemaName}';`;
 };
