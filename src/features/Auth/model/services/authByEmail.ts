088d579c763d195930b050cfa1ee0cfd829eca7a
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-01-05 15:37:15 +0300

    feat: добавил функцию helper setUserData

--- src/features/Auth/model/services/authByEmail.ts
+++ src/features/Auth/model/services/authByEmail.ts
@@ -1,11 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { User, UserActions } from '@/entities/User';
+import { User } from '@/entities/User';
 import axios from 'axios';
-import {
-  ACCESS_TOKEN_KEY,
-  REFRESH_TOKEN_KEY,
-  TOKEN_TYPE_KEY,
-} from '@/shared/consts/localstorage';
+import { setUserData } from '@/shared/lib/helpers/setUserData';
 
 interface authByEmailProps {
   email: string;
@@ -31,23 +27,7 @@ export const authByEmail = createAsyncThunk<
       'http://localhost:5000/online-store/auth/login',
       { email, password },
     );
-    if (!response.data) {
-      throw new Error();
-    }
-
-    localStorage.setItem(TOKEN_TYPE_KEY, JSON.stringify(response.data['type']));
-    localStorage.setItem(
-      ACCESS_TOKEN_KEY,
-      JSON.stringify(response.data['accessToken']),
-    );
-    localStorage.setItem(
-      REFRESH_TOKEN_KEY,
-      JSON.stringify(response.data['refreshToken']),
-    );
-
-    // Полученные данные о пользователе сохраняю в state
-    thunkAPI.dispatch(UserActions.setAuthData(response.data));
-    return response.data;
+    return setUserData(response.data, thunkAPI);
   } catch (error) {
     const messages: AuthValidationErrors[] = error.response.data;
     return thunkAPI.rejectWithValue(error.response.data.message || messages);
---
+++ src/shared/lib/helpers/setUserData.ts
@@ -0,0 +1,35 @@
+import {
+  ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_KEY,
+  TOKEN_TYPE_KEY,
+} from '@/shared/consts/localstorage';
+import { User, UserActions } from '@/entities/User';
+import { GetThunkAPI } from '@reduxjs/toolkit/dist/createAsyncThunk';
+import { AuthValidationErrors } from '@/features/Auth/model/services/authByEmail';
+import { AnyAction, Dispatch } from '@reduxjs/toolkit';
+
+export const setUserData = (
+  data: User,
+  thunkAPI: GetThunkAPI<{
+    rejectValue: string | AuthValidationErrors[];
+    state?: unknown;
+    dispatch?: Dispatch<AnyAction>;
+    extra?: unknown;
+    serializedErrorType?: unknown;
+    pendingMeta?: unknown;
+    fulfilledMeta?: unknown;
+    rejectedMeta?: unknown;
+  }>,
+) => {
+  if (!data) {
+    throw new Error();
+  }
+
+  localStorage.setItem(TOKEN_TYPE_KEY, JSON.stringify(data['type']));
+  localStorage.setItem(ACCESS_TOKEN_KEY, JSON.stringify(data['accessToken']));
+  localStorage.setItem(REFRESH_TOKEN_KEY, JSON.stringify(data['refreshToken']));
+
+  // Полученные данные о пользователе сохраняю в state
+  thunkAPI.dispatch(UserActions.setAuthData(data));
+  return data;
+};
