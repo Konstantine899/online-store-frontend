e1e2632d8f65294ff6b7a049738e75df461ad2df
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-01-21 14:17:04 +0300

    refactor: classNames.ts

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,11 +2,14 @@ type Mods = Record<string, boolean | string>;
 
 export function classNames(
   cls: string,
-  mods?: Mods,
-  additional?: string[],
+  mods: Mods = {},
+  additional: Array<string | undefined> = [],
 ): string {
-  const Mods = Object.entries(mods)
+  const Mods: string[] = Object.entries(mods)
     .filter(([_, value]) => Boolean(value))
     .map(([className]) => className);
-  return [cls, ...Mods, ...additional].join(' ');
+
+  const Additional: string[] = additional.filter(Boolean);
+  console.log(Additional);
+  return [cls, ...Mods, ...Additional].join(' ');
 }
