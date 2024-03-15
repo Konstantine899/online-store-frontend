f7f4779c0f04377e8ad3f60df26020a79e3997f7
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-03-15 09:00:25 +0300

    feat: ProductDetails добавил imageWrapper

--- src/entities/Product/ui/ProductDetails/ProductDetails.tsx
+++ src/entities/Product/ui/ProductDetails/ProductDetails.tsx
@@ -42,23 +42,7 @@ export const ProductDetails = memo((props: ProductDetailsProps) => {
   const category = useSelector(getCategoryNameSelector);
   const rating = useSelector(getRating);
   const votes = useSelector(getVotes);
-  // const votes = `6540680`.split('');
-  // let result: string;
-  // votes.forEach((char, index, array) => {
-  //   if (array.length === 7) {
-  //     const firstChar = array.slice(0, 1);
-  //     firstChar.push(' ');
-  //     const thirdChar = array.slice(1, 4);
-  //     thirdChar.push(' ');
-  //     const fiveChar = array.slice(4, 7);
-  //     fiveChar.push(' ');
-  //     console.log(firstChar.concat(thirdChar).concat(fiveChar).join(''));
-  //     result = firstChar.concat(thirdChar).concat(fiveChar).join('');
-  //   }
-  // });
-
-  // console.log('result', result);
-
+  
   if (isLoading) {
     return (
       <div className={classNames(cls.ProductDetails, {}, [className])}>
@@ -82,15 +66,18 @@ export const ProductDetails = memo((props: ProductDetailsProps) => {
       {/*<p>{`Бренд:${brand}`}</p>*/}
 
       <div className={classNames(cls.ProductDetails, {}, [className])}>
-        <KitImage
-          src={`${__API_URL__}/static/${productDetails?.image}`}
-          spareImage={
-            <img
-              src={`${__API_URL__}/static/not_found_image.jpeg`}
-              alt={'not_found_image'}
-            />
-          }
-        />
+        <div className={cls.imageWrapper}>
+          <KitImage
+            className={cls.image}
+            src={`${__API_URL__}/static/${productDetails?.image}`}
+            spareImage={
+              <img
+                src={`${__API_URL__}/static/not_found_image.jpeg`}
+                alt={'not_found_image'}
+              />
+            }
+          />
+        </div>
         <Card className={cls.ProductSummaryCard} theme={CardTheme.OUTLINED}>
           <div className={cls.upp}>
             <p className={cls.price}>{`Цена:${productDetails?.price}`}</p>
