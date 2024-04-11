4c54d9d3d5f0da8503aab9372b91befe1c67b1b5
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-04-11 09:11:52 +0300

    refactor: удалил компонент ProductListPage

--- src/pages/ProductsPage/ui/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ui/ProductsPage/ProductsPage.tsx
@@ -1,12 +1,38 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { memo, Suspense } from 'react';
+import { memo, Suspense, useEffect } from 'react';
 import cls from './ProductsPage.module.scss';
 import { Page } from '@/widgets/Page';
-import { ProductsListPage } from '../ProductsListPage/ProductsListPage';
 import { ProductsListPaginate } from '../ProductsListPaginate/ProductsListPaginate';
 import { ProductsListSorting } from '../ProductsListSorting/ProductsListSorting';
 import { useSelector } from 'react-redux';
-import { getProductsListSelector } from '@/entities/Product';
+import {
+  fetchProducts,
+  FetchProductsByBrandAndCategory,
+  FetchProductsByCategory,
+  getProductsListIsLoadingSelector,
+  getProductsListSelector,
+  ProductList,
+  ProductsPageActions,
+  ProductsPageReducer,
+} from '@/entities/Product';
+import {
+  DynamicModuleLoader,
+  ReducersList,
+} from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import {
+  BrandActions,
+  BrandReducer,
+  getBrandIdSelector,
+} from '@/entities/Brand';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
+import { CategoryActions, getCategoryIdSelector } from '@/entities/Category';
+import { ISortOrder } from '@/shared/types/ISortOrder';
+import { useSearchParams } from 'react-router-dom';
+
+const initialAsyncReducersProductsListPage: ReducersList = {
+  productsList: ProductsPageReducer,
+  brand: BrandReducer,
+};
 
 interface ProductsPageProps {
   className?: string;
@@ -14,15 +40,48 @@ interface ProductsPageProps {
 
 const ProductsPage = memo((props: ProductsPageProps) => {
   const { className } = props;
-  const products = useSelector(getProductsListSelector);
 
+  const dispatch = useAppDispatch();
+  const [URLSearchParams] = useSearchParams();
+  const limit = Number(URLSearchParams.get('limit'));
+  const page = Number(URLSearchParams.get('page'));
+  const search = URLSearchParams.get('search');
+  const sort = URLSearchParams.get('sort');
+  const products = useSelector(getProductsListSelector);
+  const isLoading = useSelector(getProductsListIsLoadingSelector);
+  const categoryId = useSelector(getCategoryIdSelector);
+  const brandId = useSelector(getBrandIdSelector);
+
+  useEffect(() => {
+    dispatch(CategoryActions.initCategory());
+    dispatch(BrandActions.initBrand());
+    dispatch(ProductsPageActions.setPage(page || 1));
+    dispatch(ProductsPageActions.setLimit(limit || 5));
+    dispatch(ProductsPageActions.setSearch(search));
+    dispatch(ProductsPageActions.setSortingOrder(sort as ISortOrder));
+    dispatch(BrandActions.setBrandId(brandId));
+  }, [brandId, dispatch, limit, page, search, sort]);
+
+  useEffect(() => {
+    if (search) {
+      dispatch(fetchProducts());
+    }
+    if (categoryId !== 0) {
+      dispatch(FetchProductsByCategory({ categoryId }));
+    }
+    if (categoryId !== 0 && brandId !== 0) {
+      dispatch(FetchProductsByBrandAndCategory({ brandId, categoryId }));
+    }
+  }, [brandId, categoryId, dispatch, search]);
   return (
     <Suspense fallback={''}>
-      <Page className={classNames(cls.ProductsPage, {}, [className])}>
-        {products.length ? <ProductsListSorting /> : null}
-        <ProductsListPage />
-        {products.length ? <ProductsListPaginate /> : null}
-      </Page>
+      <DynamicModuleLoader reducers={initialAsyncReducersProductsListPage}>
+        <Page className={classNames(cls.ProductsPage, {}, [className])}>
+          {products.length ? <ProductsListSorting /> : null}
+          <ProductList products={products} isLoading={isLoading} />
+          {products.length ? <ProductsListPaginate /> : null}
+        </Page>
+      </DynamicModuleLoader>
     </Suspense>
   );
 });
