e796ef2a59551ca715ab5a81269a080e23fe086e
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-04-24 15:35:18 +0300

    refactor: ProductSortingLimit убрал сортировку по всем продуктам

--- src/entities/Product/ui/ProductSortingLimit/ProductSortingLimit.tsx
+++ src/entities/Product/ui/ProductSortingLimit/ProductSortingLimit.tsx
@@ -20,7 +20,6 @@ import { useDebounce } from '@/shared/lib/hooks/useDebounce';
 import { getBrandIdSelector } from '@/entities/Brand';
 import { getCategoryIdSelector } from '@/entities/Category';
 import { FetchProductsByCategory } from '../../model/services/FetchProductsByCategory';
-import { fetchProducts } from '../../model/services/fetchProducts';
 import { FetchProductsByBrandAndCategory } from '../../model/services/FetchProductsByBrandAndCategory';
 
 interface SortingLimitProps {
@@ -50,7 +49,6 @@ export const ProductSortingLimit = memo((props: SortingLimitProps) => {
     if (categoryId && brandId) {
       dispatch(FetchProductsByBrandAndCategory({ categoryId, brandId }));
     }
-    dispatch(fetchProducts());
   }, [brandId, categoryId, dispatch]);
 
   const debounceLimitOrder = useDebounce(fetchProductsList, 500);
