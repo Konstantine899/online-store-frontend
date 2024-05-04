36fee675a4210e2331121f1d09b3297a5a54c4fd
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-05-04 20:19:42 +0300

    feat: storybook StoreDecorator

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -3,18 +3,19 @@ import { Provider } from 'react-redux';
 import { createReduxStore } from '@/app/providers/StoreProvider/config/store';
 import { StateSchema } from '@/app/providers/StoreProvider/config/StateSchema';
 import { ReducersMapObject } from 'redux';
+import { DeepPartial } from '@reduxjs/toolkit';
 
 export interface StoreProviderProps {
   children?: ReactNode;
-  initialState?: StateSchema;
-  asyncReducers?: ReducersMapObject<StateSchema>;
+  initialState?: DeepPartial<StateSchema>;
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
   const { children, initialState, asyncReducers } = props;
 
   const store = createReduxStore(
-    initialState,
+    initialState as StateSchema,
     asyncReducers as ReducersMapObject<StateSchema>,
   );
 
---
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -0,0 +1,28 @@
+import { PartialStoryFn } from '@storybook/csf';
+import { StoreProvider } from '@/app/providers/StoreProvider';
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from '@/app/providers/StoreProvider/config/StateSchema';
+import { ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { ReactRenderer } from '@storybook/react';
+
+const defaultAsyncReducers: ReducersList = {};
+
+export const StoreDecorator =
+  (state: DeepPartial<StateSchema>, asyncReducers?: ReducersList) =>
+  (
+    story: PartialStoryFn<
+      ReactRenderer,
+      {
+        className?: string;
+      }
+    >,
+  ) => {
+    return (
+      <StoreProvider
+        initialState={state}
+        asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}
+      >
+        {story()}
+      </StoreProvider>
+    );
+  };
