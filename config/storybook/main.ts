8d469bf0916adf1119d01489b831407be0f641db
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-05-05 13:52:41 +0300

    feat: storybook webpack добавил DefinePlugin

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import webpack, { DefinePlugin } from 'webpack';
+
 import { BuildPath } from '../build/types/config';
 import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
@@ -22,17 +24,27 @@ const config: StorybookConfig = {
     autodocs: 'tag',
   },
   features: { storyStoreV7: false },
-  webpackFinal: async (config) => {
+  webpackFinal: async (
+    config: webpack.Configuration,
+  ): Promise<webpack.Configuration> => {
     const paths: BuildPath = {
       build: '',
       html: '',
       entry: '',
       src: path.resolve(__dirname, '..', '..', 'src'),
     };
-    config.resolve.modules.push(paths.src);
-    config.resolve.extensions.push('ts', 'tsx');
-    config.resolve.alias = { '@': paths.src };
-    config.module.rules.push(buildCssLoader(true));
+    config.resolve!.modules!.push(paths.src);
+    config.resolve!.extensions!.push('ts', 'tsx');
+    config.resolve!.alias = { ...config.resolve!.alias, '@': paths.src };
+    config.module!.rules!.push(buildCssLoader(true));
+
+    config.plugins!.push(
+      new DefinePlugin({
+        __IS_DEV__: JSON.stringify(true),
+        __API_URL__: JSON.stringify(`random string`),
+        __PROJECT__: JSON.stringify('storybook'), // указываю в каком режиме запускаю
+      }),
+    );
     return config;
   },
 };
