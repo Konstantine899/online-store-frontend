ef5559fe7623f7c0aff5f1c65598ec4796d79bed
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-05-06 12:15:24 +0300

    feat: Card.stories.tsx

--- src/shared/ui/Card/Card.stories.tsx
+++ src/shared/ui/Card/Card.stories.tsx
@@ -1,16 +1,25 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Card } from './Card';
+import { Card, CardTheme } from './Card';
 
 const meta: Meta<typeof Card> = {
   title: 'entities/Card',
   component: Card,
+  args: { children: <div style={{ padding: '5px' }}>Карточка</div> },
 };
 
 export default meta;
 type Story = StoryObj<typeof Card>;
 
-export const Primary: Story = {
-  args: {},
-  render: () => <Card />,
+export const Outline: Story = {
+  args: {
+    theme: CardTheme.OUTLINED,
+  },
+  render: (args) => <Card {...args} />,
 };
-  
+
+export const OutlineActive: Story = {
+  args: {
+    theme: CardTheme.OUTLINED_ACTIVE,
+  },
+  render: (args) => <Card {...args} />,
+};
