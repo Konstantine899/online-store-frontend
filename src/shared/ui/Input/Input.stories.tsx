e731992d764b882b2c320fc2aa4ec420b01a02e5
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-05-07 14:38:58 +0300

    feat: storybook Input story OutlineBottomPassword add svg

--- src/shared/ui/Input/Input.stories.tsx
+++ src/shared/ui/Input/Input.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Input, InputTheme } from './Input';
 import { action } from '@storybook/addon-actions';
+import EyeOpen from '@/shared/assets/icons/eye-open.svg';
 
 const meta: Meta<typeof Input> = {
   title: 'shared/Input',
@@ -49,6 +50,7 @@ export const OutlineBottomPassword: Story = {
     label: 'Password',
     htmlFor: 'Password',
     required: true,
+    Svg: EyeOpen,
   },
   render: (args) => (
     <div style={{ position: 'relative', marginTop: '30px', width: '400px' }}>
