96d0f602db088b97906ed2f62f8f3219913eb786
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-05-08 12:22:57 +0300

    feat: storybook Thumb

--- src/shared/ui/Thumb/Thumb.stories.tsx
+++ src/shared/ui/Thumb/Thumb.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Thumb } from './Thumb';
+import { Thumb, ThumbSize } from './Thumb';
 
 const meta: Meta<typeof Thumb> = {
   title: 'shared/Thumb',
@@ -10,7 +10,22 @@ const meta: Meta<typeof Thumb> = {
 export default meta;
 type Story = StoryObj<typeof Thumb>;
 
-export const Primary: Story = {
-  args: {},
-  render: () => <Thumb />,
+export const ThumbS: Story = {
+  args: { size: ThumbSize.S },
+  render: (args) => <Thumb {...args} />,
+};
+
+export const ThumbM: Story = {
+  args: { size: ThumbSize.M },
+  render: (args) => <Thumb {...args} />,
+};
+
+export const ThumbL: Story = {
+  args: { size: ThumbSize.L },
+  render: (args) => <Thumb {...args} />,
+};
+
+export const ThumbXL: Story = {
+  args: { size: ThumbSize.XL },
+  render: (args) => <Thumb {...args} />,
 };
