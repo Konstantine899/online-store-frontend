c3ac7dec1d5743349426bc534d291555eaee6839
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-05-08 12:25:21 +0300

    feat: storybook Star

--- src/shared/ui/Star/Star.stories.tsx
+++ src/shared/ui/Star/Star.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Star } from './Star';
+import { Star, StarSize } from './Star';
 
 const meta: Meta<typeof Star> = {
   title: 'shared/Star',
@@ -10,7 +10,22 @@ const meta: Meta<typeof Star> = {
 export default meta;
 type Story = StoryObj<typeof Star>;
 
-export const Primary: Story = {
-  args: {},
-  render: () => <Star />,
+export const StarS: Story = {
+  args: { size: StarSize.S },
+  render: (args) => <Star {...args} />,
+};
+
+export const StarM: Story = {
+  args: { size: StarSize.M },
+  render: (args) => <Star {...args} />,
+};
+
+export const StarL: Story = {
+  args: { size: StarSize.L },
+  render: (args) => <Star {...args} />,
+};
+
+export const StarXL: Story = {
+  args: { size: StarSize.XL },
+  render: (args) => <Star {...args} />,
 };
