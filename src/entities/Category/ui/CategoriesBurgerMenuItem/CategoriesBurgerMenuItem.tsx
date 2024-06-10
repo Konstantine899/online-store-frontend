69427e37edf87c948f730e8089ece3b796459b4f
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-06-10 12:24:01 +0300

    refactor: CategoriesBurgerMenuItem.tsx добавил navigate

--- src/entities/Category/ui/CategoriesBurgerMenuItem/CategoriesBurgerMenuItem.tsx
+++ src/entities/Category/ui/CategoriesBurgerMenuItem/CategoriesBurgerMenuItem.tsx
@@ -1,7 +1,6 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { memo } from 'react';
 import cls from './CategoriesBurgerMenuItem.module.scss';
-import { AppLink } from '@/shared/ui/AppLink';
 import { CategoriesBurgerMenuItemIcon } from '../CategoriesBurgerMenuItemIcon/CategoriesBurgerMenuItemIcon';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
 import { fetchProductsByCategory, ProductsActions } from '@/entities/Product';
@@ -11,6 +10,7 @@ import { selectCategoryId } from '../../model/selectors/selectCategory';
 import { getRouteListProductsByCategory } from '@/shared/consts/router/publicRouter';
 import { BrandActions } from '@/entities/Brand';
 import { ICategory } from '../../model/types/ICategory';
+import { useNavigate } from 'react-router-dom';
 
 interface BurgerMenuItemProps {
   item: ICategory;
@@ -22,32 +22,27 @@ export const CategoriesBurgerMenuItem = memo((props: BurgerMenuItemProps) => {
   const { className, item, onClose } = props;
   const dispatch = useAppDispatch();
   const categoryId = useSelector(selectCategoryId);
+  const navigate = useNavigate();
 
   const onHandleClick = (categoryId: number) => () => {
     dispatch(ProductsActions.setPage(1));
     dispatch(CategoryActions.setCategoryId(categoryId));
     dispatch(BrandActions.setBrandId(0));
     dispatch(fetchProductsByCategory({ categoryId }));
+    navigate(getRouteListProductsByCategory(`${categoryId}`));
     onClose?.();
   };
 
   const isActive = categoryId === item.id;
   return (
-    <AppLink
-      to={getRouteListProductsByCategory(`${item.id}`)}
+    <li
+      className={classNames(cls.BurgerMenuItem, { [cls.active]: isActive }, [
+        className,
+      ])}
       onClick={onHandleClick(item.id)}
     >
-      <li
-        className={classNames(cls.BurgerMenuItem, { [cls.active]: isActive }, [
-          className,
-        ])}
-      >
-        <CategoriesBurgerMenuItemIcon
-          itemName={item.name}
-          isActive={isActive}
-        />
-        {item.name}
-      </li>
-    </AppLink>
+      <CategoriesBurgerMenuItemIcon itemName={item.name} isActive={isActive} />
+      {item.name}
+    </li>
   );
 });
