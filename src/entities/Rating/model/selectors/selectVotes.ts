745ab8174d88a896847151d62b1cb84a74cb4869
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-06-14 17:14:35 +0300

    refactor: entity rating перенес transformVotes

--- src/entities/Rating/model/selectors/selectVotes.ts
+++ src/entities/Rating/model/selectors/selectVotes.ts
@@ -1,5 +1,5 @@
 import { StateSchema } from '@/app/providers/StoreProvider/config/StateSchema';
-import { transformVotes } from '@/shared/lib/helpers/transformVotes';
+import { transformVotes } from '../../lib/helpers/transformVotes';
 
 export const selectVotes = (state: StateSchema) => {
   return transformVotes(`${state.rating?.rating?.votes ?? 0}`);
