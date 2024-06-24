08bb7fb6e643b5f1b1a500f657476989572056cd
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-06-24 10:55:07 +0300

    refactor: selectScrollPosition

--- src/features/Scroll/models/selectors/selectScroll.ts
+++ src/features/Scroll/models/selectors/selectScroll.ts
@@ -1,10 +1,11 @@
 import { StateSchema } from '@/app/providers/StoreProvider/config/StateSchema';
 import { createSelector } from '@reduxjs/toolkit';
+import { TScroll } from '../types/ScrollSchema';
 
 export const selectScroll = (state: StateSchema) => state.scroll.scroll;
 
 export const selectScrollPosition = createSelector(
   selectScroll,
   (state: StateSchema, path: string) => path, // если selector принимает несколько аргументов, то обязательно возвращаю его
-  (scroll, path) => scroll[path] || 0,
+  (scroll: TScroll, path: string) => scroll[path] || 0,
 );
