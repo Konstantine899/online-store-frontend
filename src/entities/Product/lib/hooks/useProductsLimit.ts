6e9aa789fff502d8857bf747892d845e18743133
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-07-23 21:28:56 +0300

    feat: Добавил useProductsLimit

---
+++ src/entities/Product/lib/hooks/useProductsLimit.ts
@@ -0,0 +1,66 @@
+import { useCallback, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { selectProductsLimit } from '../../model/selectors/selectProducts';
+import { TSortLimit } from '../../model/types/IProductsSchema';
+import { SelectOptions } from '@/shared/ui/Select/Select/Select';
+import { useDebounce } from '@/shared/lib/hooks/useDebounce';
+import { useSearchParams } from 'react-router-dom';
+import { LIMIT } from '@/shared/consts/urlParams';
+
+interface UseProductsLimit {
+  onFetchCb: (limit: number) => void;
+  onChangeCb: (value: TSortLimit) => void;
+}
+
+export function useProductsLimit({ onFetchCb, onChangeCb }: UseProductsLimit): {
+  selectOptions: SelectOptions<TSortLimit>[];
+  onChange: (value: TSortLimit) => void;
+  limit: TSortLimit;
+} {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const urlParamLimit = searchParams.get(LIMIT) as TSortLimit;
+  const limit = useSelector(selectProductsLimit);
+
+  const selectOptions = useMemo<SelectOptions<TSortLimit>[]>(
+    () => [
+      { value: `5`, content: '5' },
+      { value: `10`, content: '10' },
+      { value: `20`, content: '20' },
+    ],
+    [],
+  );
+
+  const addLimitToUrlParam = useCallback(
+    (limit: number) => {
+      if (limit > 5) {
+        searchParams.set(LIMIT, `${limit}`);
+        setSearchParams(searchParams);
+      } else {
+        searchParams.delete(LIMIT);
+        setSearchParams(searchParams);
+      }
+    },
+    [searchParams, setSearchParams],
+  );
+
+  const onFetch = useCallback(() => {
+    onFetchCb(limit);
+  }, [onFetchCb, limit]);
+
+  const debounce = useDebounce(onFetch, 500);
+
+  const onChange = useCallback(
+    (value: TSortLimit) => {
+      onChangeCb(value);
+      addLimitToUrlParam(Number(value));
+      debounce();
+    },
+    [addLimitToUrlParam, debounce, onChangeCb],
+  );
+
+  const isLimit = urlParamLimit
+    ? urlParamLimit
+    : (limit.toString() as TSortLimit);
+
+  return { selectOptions, onChange, limit: isLimit };
+}
--- src/entities/Product/ui/ProductsLimit/ProductsLimit.tsx
+++ src/entities/Product/ui/ProductsLimit/ProductsLimit.tsx
@@ -1,21 +1,16 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { memo, useCallback, useMemo } from 'react';
+import { memo } from 'react';
 import cls from './ProductsLimit.module.scss';
-import { useSelector } from 'react-redux';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
 import {
   Select,
-  SelectOptions,
   SelectWidth,
   WrapperWidth,
 } from '@/shared/ui/Select/Select/Select';
-import { useDebounce } from '@/shared/lib/hooks/useDebounce';
-import { selectProductsLimit } from '../../model/selectors/selectProducts';
 import { ProductsActions } from '../../model/slices/ProductsSlice';
 import { useProducts } from '../../api/productsApi';
 import { TSortLimit } from '../../model/types/IProductsSchema';
-import { useSearchParams } from 'react-router-dom';
-import { LIMIT } from '@/shared/consts/urlParams';
+import { useProductsLimit } from '../../lib/hooks/useProductsLimit';
 
 interface ProductsLimitProps {
   className?: string;
@@ -24,56 +19,27 @@ interface ProductsLimitProps {
 export const ProductsLimit = memo((props: ProductsLimitProps) => {
   const { className } = props;
   const dispatch = useAppDispatch();
-  const limit = useSelector(selectProductsLimit).toString() as TSortLimit;
   const [fetchProducts] = useProducts();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const urlParamLimit = searchParams.get(LIMIT) as TSortLimit;
 
-  const selectOptions = useMemo<SelectOptions<TSortLimit>[]>(
-    () => [
-      { value: `5`, content: '5' },
-      { value: `10`, content: '10' },
-      { value: `20`, content: '20' },
-    ],
-    [],
-  );
+  const { onChange, selectOptions, limit } = useProductsLimit({
+    onFetchCb: fetchCb,
+    onChangeCb: onChangeCb,
+  });
 
-  const addLimitToUrlParam = useCallback(
-    (limit: number) => {
-      if (limit > 5) {
-        searchParams.set(LIMIT, `${limit}`);
-        setSearchParams(searchParams);
-      } else {
-        searchParams.delete(LIMIT);
-        setSearchParams(searchParams);
-      }
-    },
-    [searchParams, setSearchParams],
-  );
-
-  const fetchProductsList = useCallback(() => {
+  function fetchCb(limit: number) {
     fetchProducts({ limit: Number(limit) });
-  }, [fetchProducts, limit]);
+  }
 
-  const debounceLimitOrder = useDebounce(fetchProductsList, 500);
-
-  const onChange = useCallback(
-    (value: TSortLimit) => {
-      dispatch(ProductsActions.setLimit(Number(value)));
-      dispatch(ProductsActions.setPage(1));
-      addLimitToUrlParam(Number(value));
-      debounceLimitOrder();
-    },
-    [addLimitToUrlParam, debounceLimitOrder, dispatch],
-  );
-
-  const isLimit = urlParamLimit ? urlParamLimit : limit;
+  function onChangeCb(value: TSortLimit) {
+    dispatch(ProductsActions.setLimit(Number(value)));
+    dispatch(ProductsActions.setPage(1));
+  }
 
   return (
     <div className={classNames(cls.SortingLimit, {}, [className])}>
       <Select
         options={selectOptions}
-        active={isLimit}
+        active={`${limit}` as TSortLimit}
         onChange={onChange}
         label={'Показывать по'}
         WrapperWidth={WrapperWidth.XL}
