64f1d03d21b9536f4b74a7e23795f44593cb72f9
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-07-25 10:59:26 +0300

    refactor: ProductsByCategoryLimit добавил useProductsLimit

--- src/entities/Product/lib/contexts/ProductsByCategoryContext.tsx
+++ src/entities/Product/lib/contexts/ProductsByCategoryContext.tsx
@@ -2,7 +2,7 @@ import { createContext, ReactNode, useContext, useEffect } from 'react';
 
 import { useParams } from 'react-router';
 import { useSelector } from 'react-redux';
-import { IProductsSchema } from '../../model/types/IProductsSchema';
+import { IProductsSchema, TSortLimit } from '../../model/types/IProductsSchema';
 import {
   selectProductsByCategoryCurrentPage,
   selectProductsByCategoryLimit,
@@ -11,6 +11,8 @@ import {
 import { useProductsByCategory } from '../../api/productsApi';
 import { ProductsActions } from '../../model/slices/ProductsSlice';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
+import { useSearchParams } from 'react-router-dom';
+import { LIMIT } from '@/shared/consts/urlParams';
 
 interface IProps {
   children: ReactNode;
@@ -38,16 +40,20 @@ export const ProductsByCategoryProvider = ({ children }: IProps) => {
   const page = useSelector(selectProductsByCategoryCurrentPage);
   const [fetchProductsByCategory, { data, isSuccess, isLoading }] =
     useProductsByCategory();
+  const [searchParams] = useSearchParams();
+  const urlParamLimit = searchParams.get(LIMIT) as TSortLimit;
+
+  const isLimit = urlParamLimit ? urlParamLimit : limit;
 
   useEffect(() => {
     dispatch(ProductsActions.setSearch(''));
     fetchProductsByCategory({
       categoryId: Number(categoryId),
-      limit,
+      limit: Number(isLimit),
       sort,
       page,
     });
-  }, [categoryId, dispatch, fetchProductsByCategory, limit, page, sort]);
+  }, [categoryId, dispatch, fetchProductsByCategory, isLimit, page, sort]);
 
   return (
     <ProductsByCategoryContext.Provider
--- src/entities/Product/ui/ProductsByCategoryLimit/ProductsByCategoryLimit.tsx
+++ src/entities/Product/ui/ProductsByCategoryLimit/ProductsByCategoryLimit.tsx
@@ -1,20 +1,19 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { memo, useCallback, useMemo } from 'react';
+import { memo } from 'react';
 import cls from './ProductsByCategoryLimit.module.scss';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
 import { useSelector } from 'react-redux';
 import { selectCategoryId } from '@/entities/Category';
 import {
   Select,
-  SelectOptions,
   SelectWidth,
   WrapperWidth,
 } from '@/shared/ui/Select/Select/Select';
-import { useDebounce } from '@/shared/lib/hooks/useDebounce';
 import { selectProductsByCategoryLimit } from '../../model/selectors/selectProductsByCategory';
 import { ProductsByCategoryActions } from '../../model/slices/ProductsByCategorySlice';
 import { useProductsByCategory } from '../../api/productsApi';
 import { TSortLimit } from '../../model/types/IProductsSchema';
+import { useProductsLimit } from '../../lib/hooks/useProductsLimit';
 
 interface ProductsByCategoryLimitProps {
   className?: string;
@@ -25,30 +24,24 @@ export const ProductsByCategoryLimit = memo(
     const { className } = props;
 
     const dispatch = useAppDispatch();
-    const limit = useSelector(selectProductsByCategoryLimit);
+    const productsByCategoryLimit = useSelector(selectProductsByCategoryLimit);
     const categoryId = useSelector(selectCategoryId);
     const [fetchProductsByCategory] = useProductsByCategory();
 
-    const selectOptions = useMemo<SelectOptions<TSortLimit>[]>(
-      () => [
-        { value: `5`, content: '5' },
-        { value: `10`, content: '10' },
-        { value: `20`, content: '20' },
-      ],
-      [],
-    );
+    const { limit, onChange, selectOptions } = useProductsLimit({
+      onFetchCb,
+      onChangeCb,
+      limit: productsByCategoryLimit,
+    });
 
-    const fetchProductsList = useCallback(() => {
-      fetchProductsByCategory({ categoryId, limit });
-    }, [categoryId, fetchProductsByCategory, limit]);
+    function onFetchCb() {
+      fetchProductsByCategory({ categoryId, limit: Number(limit) });
+    }
 
-    const debounceLimitOrder = useDebounce(fetchProductsList, 500);
-
-    const onChange = (value: TSortLimit) => {
-      dispatch(ProductsByCategoryActions.setLimit(Number(value)));
+    function onChangeCb(limit: TSortLimit) {
+      dispatch(ProductsByCategoryActions.setLimit(Number(limit)));
       dispatch(ProductsByCategoryActions.setPage(1));
-      debounceLimitOrder();
-    };
+    }
 
     return (
       <div className={classNames(cls.ProductsByCategoryLimit, {}, [className])}>
