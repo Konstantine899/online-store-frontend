4cd89f684588dc0d5ead45ca852e806c3b0912ad
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-08-03 15:35:35 +0300

    feat:ProductsByCategoryContext обновление страницы
    При обновлении страницы из url подгружаю limit sort page

--- src/entities/Product/lib/contexts/ProductsByCategoryContext.tsx
+++ src/entities/Product/lib/contexts/ProductsByCategoryContext.tsx
@@ -2,11 +2,7 @@ import { createContext, ReactNode, useContext, useEffect } from 'react';
 
 import { useParams } from 'react-router';
 import { useSelector } from 'react-redux';
-import {
-  IProductsSchema,
-  TSortLimit,
-  TSortOrder,
-} from '../../model/types/IProductsSchema';
+import { IProductsSchema } from '../../model/types/IProductsSchema';
 import {
   selectProductsByCategoryCurrentPage,
   selectProductsByCategoryLimit,
@@ -15,8 +11,9 @@ import {
 import { useProductsByCategory } from '../../api/productsApi';
 import { ProductsActions } from '../../model/slices/ProductsSlice';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
-import { useSearchParams } from 'react-router-dom';
-import { LIMIT, SORT } from '@/shared/consts/urlParams';
+import { useAddLimitToUrlParam } from '../hooks/useAddLimitToUrlParam';
+import { useAddSortOrderToUrlParam } from '../hooks/useAddSortOrderToUrlParam';
+import { useAddCurrentPageToUrlParam } from '../hooks/useAddCurrentPageToUrlParam';
 
 interface IProps {
   children: ReactNode;
@@ -39,34 +36,25 @@ export const useProductsByCategoryContext = () =>
 export const ProductsByCategoryProvider = ({ children }: IProps) => {
   const { categoryId } = useParams();
   const dispatch = useAppDispatch();
-  const limit = useSelector(selectProductsByCategoryLimit);
-  const sort = useSelector(selectProductsByCategorySort);
-  const page = useSelector(selectProductsByCategoryCurrentPage);
+  const limitFromState = useSelector(selectProductsByCategoryLimit);
+  const sortFromState = useSelector(selectProductsByCategorySort);
+  const pageFromState = useSelector(selectProductsByCategoryCurrentPage);
   const [fetchProductsByCategory, { data, isSuccess, isLoading }] =
     useProductsByCategory();
-  const [searchParams] = useSearchParams();
-  const urlParamLimit = searchParams.get(LIMIT) as TSortLimit;
-  const urlParamSortOrder = searchParams.get(SORT) as TSortOrder;
-  const isSortOrder = urlParamSortOrder ? urlParamSortOrder : sort;
 
-  const isLimit = urlParamLimit ? urlParamLimit : limit;
+  const { limit } = useAddLimitToUrlParam(limitFromState);
+  const { sort } = useAddSortOrderToUrlParam(sortFromState);
+  const { page } = useAddCurrentPageToUrlParam(pageFromState);
 
   useEffect(() => {
     dispatch(ProductsActions.setSearch(''));
     fetchProductsByCategory({
       categoryId: Number(categoryId),
-      limit: Number(isLimit),
-      sort: isSortOrder,
+      limit: Number(limit),
+      sort,
       page,
     });
-  }, [
-    categoryId,
-    dispatch,
-    fetchProductsByCategory,
-    isLimit,
-    isSortOrder,
-    page,
-  ]);
+  }, [categoryId, dispatch, fetchProductsByCategory, limit, sort, page]);
 
   return (
     <ProductsByCategoryContext.Provider
