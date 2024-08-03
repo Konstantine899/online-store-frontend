7b0e3e6dae370b0cf934e692476ecb3dea8dabcc
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-08-03 15:17:51 +0300

    feat:ProductsByCategoryPaginate
    onPageChange доавляю в url sort limit page

--- src/entities/Product/ui/ProductsByCategoryPaginate/ProductsByCategoryPaginate.tsx
+++ src/entities/Product/ui/ProductsByCategoryPaginate/ProductsByCategoryPaginate.tsx
@@ -13,6 +13,14 @@ import { useProductsByCategory } from '../../api/productsApi';
 import { useProductsByCategoryContext } from '../../lib/contexts/ProductsByCategoryContext';
 import { ProductsByCategoryActions } from '../../model/slices/ProductsByCategorySlice';
 
+import {
+  selectProductsByCategoryLimit,
+  selectProductsByCategorySort,
+} from '../../model/selectors/selectProductsByCategory';
+import { useAddSortOrderToUrlParam } from '../../lib/hooks/useAddSortOrderToUrlParam';
+import { useAddLimitToUrlParam } from '../../lib/hooks/useAddLimitToUrlParam';
+import { useAddCurrentPageToUrlParam } from '../../lib/hooks/useAddCurrentPageToUrlParam';
+
 interface ProductsByCategoryPaginateProps {
   className?: string;
 }
@@ -25,16 +33,34 @@ export const ProductsByCategoryPaginate = memo(
     const navigate = useNavigate();
     const [fetchProductsByCategory] = useProductsByCategory();
     const { productsByCategory, isSuccess } = useProductsByCategoryContext();
+    const sortFromState = useSelector(selectProductsByCategorySort);
+    const limitFromState = useSelector(selectProductsByCategoryLimit);
+
+    const currentPage = productsByCategory
+      ? productsByCategory.metaData.currentPage
+      : 1;
+    const lastPage = productsByCategory
+      ? productsByCategory.metaData.lastPage
+      : 1;
+
+    const { sort, addSortOrderToUrlParam } =
+      useAddSortOrderToUrlParam(sortFromState);
+    const { limit, addLimitToUrlParam } = useAddLimitToUrlParam(limitFromState);
+    const { addCurrentPageToUrlParam } =
+      useAddCurrentPageToUrlParam(currentPage);
 
     const onPageChange = (pageNumber: number) => {
       dispatch(ProductsByCategoryActions.setPage(pageNumber));
       navigate(getRouteProductsByCategory(`${categoryId}`));
       fetchProductsByCategory({ categoryId, page: pageNumber });
+      addSortOrderToUrlParam(sort);
+      addLimitToUrlParam(Number(limit));
+      addCurrentPageToUrlParam(pageNumber);
     };
 
     const paginationRange = usePaginate({
-      currentPage: productsByCategory?.metaData.currentPage,
-      lastPage: productsByCategory?.metaData.lastPage,
+      currentPage,
+      lastPage,
     });
 
     if (isSuccess && productsByCategory) {
@@ -47,8 +73,8 @@ export const ProductsByCategoryPaginate = memo(
           <Paginate
             onPageChange={onPageChange}
             paginationRange={paginationRange}
-            currentPage={productsByCategory.metaData.currentPage}
-            lastPage={productsByCategory.metaData.lastPage}
+            currentPage={currentPage}
+            lastPage={lastPage}
           />
         </div>
       );
