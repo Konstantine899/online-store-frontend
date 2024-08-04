cda3b9193b1996be6d6b23d610752aa5659a40fd
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-08-04 16:48:18 +0300

    refaсtor: Удалил selectBrand
    Из entity Brand удалил селектор selectBrand

--- src/entities/Brand/index.ts
+++ src/entities/Brand/index.ts
@@ -5,9 +5,6 @@ export { BrandTabs } from './ui/BrandTabs/BrandTabs';
 export { BrandActions } from './model/slices/BrandSlice';
 export { brandReducers } from './model/slices';
 
-// selectors
-export { selectBrandId } from './model/selectors/selectBrand';
-
 // types
 export type { IBrandSchema } from './model/types/IBrandSchema';
 export type { IBrand } from './model/types/IBrand';
