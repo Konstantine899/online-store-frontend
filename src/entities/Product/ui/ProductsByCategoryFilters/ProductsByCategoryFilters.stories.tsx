da3dd0837a072ef1b5ceb5e68ab685121af911bf
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-08-19 12:54:43 +0300

    feat: ProductsByCategoryFilters
    Добавил stories cases Pending и Success

--- src/entities/Product/ui/ProductsByCategoryFilters/ProductsByCategoryFilters.stories.tsx
+++ src/entities/Product/ui/ProductsByCategoryFilters/ProductsByCategoryFilters.stories.tsx
@@ -1,15 +1,43 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ProductsByCategoryFilters } from './ProductsByCategoryFilters';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { handlers } from '@/shared/config/storybook/mocks/handlers';
 
 const meta: Meta<typeof ProductsByCategoryFilters> = {
-  title: 'stories/ProductsByCategoryFilters',
+  title: 'entities/ProductsByCategoryFilters',
   component: ProductsByCategoryFilters,
+  decorators: [
+    StoreDecorator({
+      category: { category: { id: 1, name: 'Смартфоны', image: '' } },
+    }),
+  ],
 };
 
 export default meta;
 type Story = StoryObj<typeof ProductsByCategoryFilters>;
 
-export const Primary: Story = {
+export const Pending: Story = {
   args: {},
   render: (args) => <ProductsByCategoryFilters {...args} />,
+  parameters: {
+    msw: {
+      handlers: [
+        handlers.productsAllByCategoryIdPending,
+        handlers.brandsListByCategoryPending,
+      ],
+    },
+  },
+};
+
+export const Success: Story = {
+  args: {},
+  render: (args) => <ProductsByCategoryFilters {...args} />,
+  parameters: {
+    msw: {
+      handlers: [
+        handlers.productsAllByCategoryIdSuccess,
+        handlers.brandsListByCategorySuccess,
+      ],
+    },
+  },
 };
