d38d56a7991173c7071f7deb425756a584113784
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-08-19 14:05:28 +0300

    refactor: NotFoundPage добавил разметку

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,6 +1,10 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { memo } from 'react';
 import cls from './NotFoundPage.module.scss';
+import { Text } from '@/shared/ui/Text';
+import { TextAlign, TextSize, TextTheme } from '@/shared/ui/Text/Text';
+import { KitImage } from '@/shared/ui/KitImage/KitImage';
+import { getRouteImage } from '@/shared/consts/router/publicRouter';
 
 interface NotFoundPageProps {
   className?: string;
@@ -10,7 +14,18 @@ export const NotFoundPage = memo((props: NotFoundPageProps) => {
   const { className } = props;
 
   return (
-    <div className={classNames(cls.NotFoundPage, {}, [className])}>{}</div>
+    <div className={classNames(cls.NotFoundPage, {}, [className])}>
+      <div className={cls.rebootImg}>
+        <KitImage src={getRouteImage(`reboot.gif`)} height={300} width={406} />
+      </div>
+      <Text
+        theme={TextTheme.INVERTED_DISABLED}
+        title={'Страница не найдена'}
+        text={'Что бы найти товары воспользуйтесь поиском'}
+        size={TextSize.L}
+        align={TextAlign.CENTER}
+      />
+    </div>
   );
 });
 
