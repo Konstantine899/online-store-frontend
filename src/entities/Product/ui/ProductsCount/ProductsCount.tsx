4f8f67458b0a8fa1cd62b924021e2bf545e3bf04
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-08-19 15:45:45 +0300

    feat:ProductsCount
    Добавил состояния isLoading и isSuccess

--- src/entities/Product/ui/ProductsCount/ProductsCount.tsx
+++ src/entities/Product/ui/ProductsCount/ProductsCount.tsx
@@ -4,6 +4,7 @@ import cls from './ProductsCount.module.scss';
 import { Text } from '@/shared/ui/Text';
 import { TextSize, TextTheme } from '@/shared/ui/Text/Text';
 import { useProductsContext } from '../../lib/contexts/ProductsContext';
+import { Skeleton } from '@/shared/ui/Skeleton';
 
 interface ProductsCountProps {
   className?: string;
@@ -11,18 +12,33 @@ interface ProductsCountProps {
 
 export const ProductsCount = memo((props: ProductsCountProps) => {
   const { className } = props;
-  const { products } = useProductsContext();
+  const { products, isSuccess, isLoading } = useProductsContext();
 
-  return (
-    <div className={classNames(cls.ProductsCount, {}, [className])}>
-      <Text text={`Все товары`} theme={TextTheme.INVERTED} size={TextSize.XL} />
-      <Text
-        text={`(${products?.count})`}
-        theme={TextTheme.INVERTED}
-        size={TextSize.XL}
-      />
-    </div>
-  );
+  if (isLoading) {
+    return (
+      <div className={classNames(cls.ProductsCount, {}, [className])}>
+        <Skeleton width={173} height={40} borderRadius={'10px'} />
+        <Skeleton width={58} height={40} borderRadius={'10px'} />
+      </div>
+    );
+  }
+
+  if (isSuccess && products!.rows.length > 0) {
+    return (
+      <div className={classNames(cls.ProductsCount, {}, [className])}>
+        <Text
+          text={`Все товары`}
+          theme={TextTheme.INVERTED}
+          size={TextSize.XL}
+        />
+        <Text
+          text={`(${products?.count})`}
+          theme={TextTheme.INVERTED}
+          size={TextSize.XL}
+        />
+      </div>
+    );
+  }
 });
 
 ProductsCount.displayName = `ProductsCount`;
