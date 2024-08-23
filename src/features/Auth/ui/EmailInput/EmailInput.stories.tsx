eaf55a04b28bae378a1f4b04142d2fe95eec2944
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-08-23 10:46:09 +0300

    feat: Добавил EmailInput stories
    Описал stories Primary и Secondary

--- src/features/Auth/ui/EmailInput/EmailInput.stories.tsx
+++ src/features/Auth/ui/EmailInput/EmailInput.stories.tsx
@@ -1,8 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { EmailInput } from './EmailInput';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 const meta: Meta<typeof EmailInput> = {
-  title: 'entities/EmailInput',
+  title: 'features/Auth/EmailInput',
   component: EmailInput,
 };
 
@@ -11,5 +12,12 @@ type Story = StoryObj<typeof EmailInput>;
 
 export const Primary: Story = {
   args: {},
-  render: () => <EmailInput />,
+  render: (args) => <EmailInput {...args} />,
+  decorators: [StoreDecorator({})],
+};
+
+export const Secondary: Story = {
+  args: {},
+  render: (args) => <EmailInput {...args} />,
+  decorators: [StoreDecorator({ auth: { email: 'test@email.com' } })],
 };
