6d1765f03600949a0a16b983ac9ca6e6d4d4d6d4
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-08-28 16:58:32 +0300

    feat:RegistrationModal stories
    Добавил story case Primary

--- src/features/Auth/ui/RegistrationModal/RegistrationModal.stories.tsx
+++ src/features/Auth/ui/RegistrationModal/RegistrationModal.stories.tsx
@@ -1,9 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { RegistrationModal } from './RegistrationModal';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { Modal } from '@/shared/ui/Modal/Modal';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './RegistrationModal.module.scss';
+import { RegistrationFormAsync as RegistrationForm } from '../RegistrationForm/RegistrationForm.async';
+import { fn } from '@storybook/test';
 
 const meta: Meta<typeof RegistrationModal> = {
   title: 'features/RegistrationModel',
   component: RegistrationModal,
+  decorators: [StoreDecorator({})],
 };
 
 export default meta;
@@ -11,5 +18,14 @@ type Story = StoryObj<typeof RegistrationModal>;
 
 export const Primary: Story = {
   args: {},
-  render: (args) => <RegistrationModal {...args} />,
+  render: () => (
+    <Modal
+      isOpen={true}
+      onClose={fn()}
+      lazy
+      className={classNames(cls.RegistrationModal, {}, [])}
+    >
+      <RegistrationForm />
+    </Modal>
+  ),
 };
