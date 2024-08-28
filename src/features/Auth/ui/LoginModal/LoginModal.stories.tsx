dc596ddaa9d922299e94bfcf14a6f3cb81b15b05
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-08-28 16:47:45 +0300

    feat: LoginModal stories
    Добавил story case Primary

--- src/features/Auth/ui/LoginModal/LoginModal.stories.tsx
+++ src/features/Auth/ui/LoginModal/LoginModal.stories.tsx
@@ -1,9 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { LoginModal } from './LoginModal';
+import { Modal } from '@/shared/ui/Modal/Modal';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './LoginModal.module.scss';
+import { LoginFormAsync as LoginForm } from '../LoginForm/LoginForm.async';
+import { fn } from '@storybook/test';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 const meta: Meta<typeof LoginModal> = {
   title: 'features/LoginModal',
   component: LoginModal,
+  decorators: [StoreDecorator({})],
 };
 
 export default meta;
@@ -11,5 +18,14 @@ type Story = StoryObj<typeof LoginModal>;
 
 export const Primary: Story = {
   args: {},
-  render: (args) => <LoginModal {...args} />,
+  render: (args) => (
+    <Modal
+      isOpen={true}
+      onClose={fn()}
+      lazy
+      className={classNames(cls.LoginModal, {}, [])}
+    >
+      <LoginForm {...args} />
+    </Modal>
+  ),
 };
