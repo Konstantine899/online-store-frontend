9422fc31494ed30e8d6ee07f3669f19f0801487c
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-08-29 11:13:34 +0300

    feat: Paginate stories
    Добавил Primary story

--- src/entities/Paginate/ui/Paginate.stories.tsx
+++ src/entities/Paginate/ui/Paginate.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Paginate } from './Paginate';
+import { fn } from '@storybook/test';
 
 const meta: Meta<typeof Paginate> = {
   title: 'entities/Paginate',
@@ -10,6 +11,11 @@ export default meta;
 type Story = StoryObj<typeof Paginate>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    paginationRange: [1, '...', 3, 4, 5, 6, 7, 8, 9, '...', 35],
+    currentPage: 6,
+    lastPage: 35,
+    onPageChange: fn(),
+  },
   render: (args) => <Paginate {...args} />,
 };
