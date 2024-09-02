b5c281a49ebb1ed6a559760f508aa6b060066df1
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-09-02 15:59:20 +0300

    feat: ProductSpecification добавил stories

--- src/entities/Product/ui/ProductSpecification/ProductSpecification.stories.tsx
+++ src/entities/Product/ui/ProductSpecification/ProductSpecification.stories.tsx
@@ -1,5 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ProductSpecification } from './ProductSpecification';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './ProductSpecification.module.scss';
+import { Text } from '@/shared/ui/Text';
+import { TextSize, TextTheme } from '@/shared/ui/Text/Text';
+import { IProductProperty } from '../../model/types/IProduct';
+import { mockProduct } from '@/shared/config/storybook/mocks/mockProduct';
 
 const meta: Meta<typeof ProductSpecification> = {
   title: 'entities/ProductSpecification',
@@ -11,5 +17,24 @@ type Story = StoryObj<typeof ProductSpecification>;
 
 export const Primary: Story = {
   args: {},
-  render: (args) => <ProductSpecification {...args} />,
+  render: () => (
+    <div className={classNames(cls.ProductSpecification, {}, [])}>
+      <Text
+        text={`Характеристики`}
+        size={TextSize.XL}
+        theme={TextTheme.INVERTED}
+        className={cls.Title}
+      />
+      <table className={cls.Table}>
+        <tbody>
+          {mockProduct.properties.map((property: IProductProperty) => (
+            <tr key={property.id}>
+              <td>{property.name}</td>
+              <td>{property.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  ),
 };
