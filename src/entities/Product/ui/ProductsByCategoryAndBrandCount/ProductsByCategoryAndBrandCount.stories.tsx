72ffe3314d6b01ce0089a564928f2c4f962c3f73
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-09-02 17:33:28 +0300

    feat: ProductsByCategoryAndBrandCount stories

--- src/entities/Product/ui/ProductsByCategoryAndBrandCount/ProductsByCategoryAndBrandCount.stories.tsx
+++ src/entities/Product/ui/ProductsByCategoryAndBrandCount/ProductsByCategoryAndBrandCount.stories.tsx
@@ -1,5 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ProductsByCategoryAndBrandCount } from './ProductsByCategoryAndBrandCount';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './ProductsByCategoryAndBrandCount.module.scss';
+import { Text } from '@/shared/ui/Text';
+import { TextSize, TextTheme } from '@/shared/ui/Text/Text';
 
 const meta: Meta<typeof ProductsByCategoryAndBrandCount> = {
   title: 'entities/ProductsByCategoryAndBrandCount',
@@ -11,5 +15,10 @@ type Story = StoryObj<typeof ProductsByCategoryAndBrandCount>;
 
 export const Primary: Story = {
   args: {},
-  render: (args) => <ProductsByCategoryAndBrandCount {...args} />,
+  render: () => (
+    <div className={classNames(cls.ProductsByCategoryAndBrandCount, {}, [])}>
+      <Text text={`Смартфоны`} theme={TextTheme.INVERTED} size={TextSize.XL} />
+      <Text text={`(10)`} theme={TextTheme.INVERTED} size={TextSize.XL} />
+    </div>
+  ),
 };
