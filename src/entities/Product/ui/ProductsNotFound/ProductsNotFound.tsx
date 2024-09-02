37c4265f481d864b8c9e3ee2de5ea882a6ea2f39
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-09-02 12:52:21 +0300

    refactor: ProductsNotFound
    Перенес карусель с продуктами
    в компонент ProductsPopular

--- src/entities/Product/ui/ProductsNotFound/ProductsNotFound.tsx
+++ src/entities/Product/ui/ProductsNotFound/ProductsNotFound.tsx
@@ -1,19 +1,7 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { memo, useEffect } from 'react';
+import { memo } from 'react';
 import cls from './ProductsNotFound.module.scss';
-import { Carousel } from '@/shared/ui/Carousel/Carousel';
-import { useProducts } from '../../api/productsApi';
-import { Card } from '@/shared/ui/Card';
-import { KitImage } from '@/shared/ui/KitImage/KitImage';
-import {
-  getRouteImage,
-  getRouteImageNotFound,
-  getRouteProduct,
-} from '@/shared/consts/router/publicRouter';
-import { Text } from '@/shared/ui/Text';
-import { TextTheme } from '@/shared/ui/Text/Text';
-import { AppLink } from '@/shared/ui/AppLink';
-import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/Button';
+import { ProductsPopular } from '@/entities/Product/ui/ProductsPopular/ProductsPopular';
 
 interface ProductsNotFoundProps {
   className?: string;
@@ -22,68 +10,11 @@ interface ProductsNotFoundProps {
 export const ProductsNotFound = memo((props: ProductsNotFoundProps) => {
   const { className } = props;
 
-  const [fetchProducts, { data, isLoading, isSuccess }] = useProducts();
-
-  useEffect(() => {
-    fetchProducts({ limit: 20 });
-  }, [fetchProducts]);
-
-  if (isLoading) {
-    return (
-      <div className={classNames(cls.ProductsNotFound, {}, [className])}></div>
-    );
-  }
-
-  if (isSuccess && data!.rows.length > 0) {
-    return (
-      <div className={classNames(cls.ProductsNotFound, {}, [className])}>
-        <Carousel elementsQuantity={5} infinite={false}>
-          {data!.rows.map((product) => (
-            <Card key={product.id} className={cls.Card}>
-              <div className={cls.Top}>
-                <AppLink to={getRouteProduct(`${product.id}`)}>
-                  <KitImage
-                    src={getRouteImage(product.image)}
-                    width={225}
-                    height={220}
-                    alt={product.image}
-                    spareImage={
-                      <img
-                        src={getRouteImageNotFound()}
-                        alt={'not_found_image'}
-                      />
-                    }
-                  />
-                </AppLink>
-              </div>
-              <div className={cls.Bottom}>
-                <AppLink to={getRouteProduct(`${product.id}`)}>
-                  <Text
-                    theme={TextTheme.INVERTED}
-                    text={`${product.name}`}
-                    ellipsis
-                  />
-                </AppLink>
-                <Text
-                  theme={TextTheme.INVERTED}
-                  text={`${product.price}`}
-                  className={cls.Price}
-                />
-                <Button
-                  className={cls.CardAdd}
-                  theme={ButtonTheme.OUTLINE}
-                  size={ButtonSize.M}
-                  fullWidth
-                >
-                  Добавить в корзину
-                </Button>
-              </div>
-            </Card>
-          ))}
-        </Carousel>
-      </div>
-    );
-  }
+  return (
+    <div className={classNames(cls.ProductsNotFound, {}, [className])}>
+      <ProductsPopular />
+    </div>
+  );
 });
 
 ProductsNotFound.displayName = `ProductsNotFound`;
---
+++ src/entities/Product/ui/ProductsPopular/ProductsPopular.tsx
@@ -0,0 +1,88 @@
+import { memo, useEffect } from 'react';
+import cls from './ProductsPopular.module.scss';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { useProducts } from '@/entities/Product';
+import { Carousel } from '@/shared/ui/Carousel/Carousel';
+import { Card } from '@/shared/ui/Card';
+import { AppLink } from '@/shared/ui/AppLink';
+import {
+  getRouteImage,
+  getRouteImageNotFound,
+  getRouteProduct,
+} from '@/shared/consts/router/publicRouter';
+import { KitImage } from '@/shared/ui/KitImage/KitImage';
+import { Text } from '@/shared/ui/Text';
+import { TextTheme } from '@/shared/ui/Text/Text';
+import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/Button';
+
+interface ProductsPopularProps {
+  className?: string;
+}
+
+export const ProductsPopular = memo((props: ProductsPopularProps) => {
+  const { className } = props;
+
+  const [fetchProducts, { data, isLoading, isSuccess }] = useProducts();
+
+  useEffect(() => {
+    fetchProducts({ limit: 20 });
+  }, [fetchProducts]);
+
+  if (isLoading) {
+    return <div></div>;
+  }
+
+  if (isSuccess && data!.rows.length > 0) {
+    return (
+      <Carousel elementsQuantity={5} infinite={false}>
+        {data!.rows.map((product) => (
+          <Card key={product.id} className={cls.Card}>
+            <div className={cls.Top}>
+              <AppLink to={getRouteProduct(`${product.id}`)}>
+                <KitImage
+                  src={getRouteImage(product.image)}
+                  width={225}
+                  height={220}
+                  alt={product.image}
+                  spareImage={
+                    <img
+                      src={getRouteImageNotFound()}
+                      alt={'not_found_image'}
+                    />
+                  }
+                />
+              </AppLink>
+            </div>
+            <div className={cls.Bottom}>
+              <AppLink to={getRouteProduct(`${product.id}`)}>
+                <Text
+                  theme={TextTheme.INVERTED}
+                  text={`${product.name}`}
+                  ellipsis
+                />
+              </AppLink>
+              <Text
+                theme={TextTheme.INVERTED}
+                text={`${product.price}`}
+                className={cls.Price}
+              />
+              <Button
+                className={cls.CardAdd}
+                theme={ButtonTheme.OUTLINE}
+                size={ButtonSize.M}
+                fullWidth
+              >
+                Добавить в корзину
+              </Button>
+            </div>
+          </Card>
+        ))}
+      </Carousel>
+    );
+  }
+  return (
+    <div className={classNames(cls.ProductsPopular, {}, [className])}></div>
+  );
+});
+
+ProductsPopular.displayName = `ProductsPopular`;
