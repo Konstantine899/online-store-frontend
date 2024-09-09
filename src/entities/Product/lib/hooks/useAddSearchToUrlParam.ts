ce10ea4cad8d52179f9961ae491d0a85b3014d1d
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-09-09 13:09:26 +0300

    feat: useAddSearchToUrlParam
    Добавляю в url limit sort page

--- src/entities/Product/lib/hooks/useAddSearchToUrlParam.ts
+++ src/entities/Product/lib/hooks/useAddSearchToUrlParam.ts
@@ -1,8 +1,14 @@
 import { useSelector } from 'react-redux';
-import { selectProductsSearch } from '../../model/selectors/selectProducts';
+import {
+  selectProductsCurrentPage,
+  selectProductsLimit,
+  selectProductsSearch,
+  selectProductsSortOrder,
+} from '../../model/selectors/selectProducts';
 import { useSearchParams } from 'react-router-dom';
-import { SEARCH } from '@/shared/consts/urlParams';
+import { LIMIT, PAGE, SEARCH, SORT } from '@/shared/consts/urlParams';
 import { useCallback } from 'react';
+import { TSortOrder } from '../../model/types/IProductsSchema';
 
 interface IUseAddSearchToUrlParamResponse {
   search: string;
@@ -11,18 +17,48 @@ interface IUseAddSearchToUrlParamResponse {
 
 export function useAddSearchToUrlParam(): IUseAddSearchToUrlParamResponse {
   const search = useSelector(selectProductsSearch);
+  const page = useSelector(selectProductsCurrentPage);
+  const limit = useSelector(selectProductsLimit);
+  const sort = useSelector(selectProductsSortOrder);
   const [searchParams, setSearchParams] = useSearchParams();
   const urlParamSearch = searchParams.get(SEARCH);
   const isSearch = urlParamSearch ? urlParamSearch : search;
+
+  const addLimit = useCallback(
+    (limit: string) => {
+      if (Number(limit) > 5) {
+        searchParams.set(LIMIT, `${limit}`);
+        setSearchParams(searchParams);
+      }
+    },
+    [searchParams, setSearchParams],
+  );
+
+  const addSort = useCallback(
+    (sort: TSortOrder) => {
+      searchParams.set(SORT, `${sort}`);
+      setSearchParams(searchParams);
+    },
+    [searchParams, setSearchParams],
+  );
 
   const addSearchToUrlParam = useCallback(
     (search: string) => {
       if (search.length > 0) {
         searchParams.set(SEARCH, search);
         setSearchParams(searchParams);
+        addLimit(`${limit}`);
+        addSort(sort);
       }
+      if (page > 1) {
+        searchParams.set(PAGE, `${page}`);
+        setSearchParams(searchParams);
+      }
+
+      addLimit(`${limit}`);
+      addSort(sort);
     },
-    [searchParams, setSearchParams],
+    [addLimit, addSort, limit, page, searchParams, setSearchParams, sort],
   );
   return { search: isSearch, addSearchToUrlParam };
 }
