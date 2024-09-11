e870e4aeedef1b1ff75ef4233961fcef618a3653
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-09-11 14:57:14 +0300

    feat: Добавил story cases в Limit
    Добавил story cases Primary и Loading

--- src/entities/Limit/ui/Limit/Limit.stories.tsx
+++ src/entities/Limit/ui/Limit/Limit.stories.tsx
@@ -1,17 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Limit } from './Limit';
+import { fn } from '@storybook/test';
 
-  import type { Meta, StoryObj } from '@storybook/react';
-  import { Limit } from './Limit';
-  
-  const meta: Meta<typeof Limit> = {
-  title: 'entities/Limit',   
+const meta: Meta<typeof Limit> = {
+  title: 'entities/Limit',
   component: Limit,
 };
-  
-  export default meta;
-  type Story = StoryObj<typeof Limit>;
-  
-  export const Primary: Story = {
-  args: {},
-  render: (args) => <Limit {...args}/>,
+
+export default meta;
+type Story = StoryObj<typeof Limit>;
+
+export const Primary: Story = {
+  args: {
+    isSuccess: true,
+    limit: '5',
+    selectOptions: [
+      { content: '5', value: '5' },
+      { content: '10', value: '10' },
+      { content: '20', value: '20' },
+    ],
+    onChange: fn(),
+  },
+  render: (args) => <Limit {...args} />,
 };
-  
+
+export const Loading: Story = {
+  args: {
+    isLoading: true,
+  },
+  render: (args) => <Limit {...args} />,
+};
