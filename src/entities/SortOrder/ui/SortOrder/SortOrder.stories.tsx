0aa61b8abb1d7880c50db4d1b6a02d5e129f6342
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-09-11 17:17:59 +0300

    feat: SortOrder добавил story cases
    Добавил story cases Primaty и Loading

--- src/entities/SortOrder/ui/SortOrder/SortOrder.stories.tsx
+++ src/entities/SortOrder/ui/SortOrder/SortOrder.stories.tsx
@@ -1,17 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { SortOrder } from './SortOrder';
+import { mockProducts } from '@/shared/config/storybook/mocks/mockProducts';
 
-  import type { Meta, StoryObj } from '@storybook/react';
-  import { SortOrder } from './SortOrder';
-  
-  const meta: Meta<typeof SortOrder> = {
-  title: 'entities/SortOrder',   
+const meta: Meta<typeof SortOrder> = {
+  title: 'entities/SortOrder',
   component: SortOrder,
 };
-  
-  export default meta;
-  type Story = StoryObj<typeof SortOrder>;
-  
-  export const Primary: Story = {
-  args: {},
-  render: (args) => <SortOrder {...args}/>,
+
+export default meta;
+type Story = StoryObj<typeof SortOrder>;
+
+export const Primary: Story = {
+  args: {
+    products: mockProducts,
+    isSuccess: true,
+    selectOptions: [
+      { value: 'asc', content: 'возрастанию' },
+      { value: 'desc', content: 'убыванию' },
+    ],
+    sort: 'asc',
+  },
+  render: (args) => <SortOrder {...args} />,
 };
-  
+
+export const Loading: Story = {
+  args: {
+    isLoading: true,
+  },
+  render: (args) => <SortOrder {...args} />,
+};
